218a56143a15e438949a87a19092353dfdccb12a
Yuliya <iksulia1@gmail.com>
2024-04-02 19:31:21 +0300

    Login

--- order_food/src/pages/Login/Login.tsx
+++ order_food/src/pages/Login/Login.tsx
@@ -1,27 +1,70 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import Heading from "../../components/Heading/Heading";
 import Input from "../../components/Input/Input";
 import styles from "./Login.module.css";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
+import axios, { AxiosError } from "axios";
+import { PREFIX } from "../../components/helpers/api";
+import { LoginResponse } from "../../interfaces/auth.interface";
+
+export type LoginForm = {
+  email: {
+    value: string;
+  };
+  password: {
+    value: string;
+  };
+};
 
 export function Login() {
-  const submit = (e: FormEvent) => {
+  const [error, setError] = useState<string | null>();
+  const navigate = useNavigate();
+
+  const submit = async (e: FormEvent) => {
     e.preventDefault();
-    console.log(e);
+    setError(null);
+    const target = e.target as typeof e.target & LoginForm;
+    const { email, password } = target;
+    console.log(email.value);
+    console.log(password.value);
+    await sendLogin(email.value, password.value);
+  };
+
+  const sendLogin = async (email: string, password: string) => {
+    try {
+      const { data } = await axios.post<LoginResponse>(`${PREFIX}users`, {
+        email,
+        password,
+      });
+      console.log(data);
+      localStorage.setItem("jwt", data.access_token);
+      navigate("/");
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        console.log(e);
+        setError(e.response?.data.message);
+      }
+    }
   };
 
   return (
-    <div className={styles["login"]} onSubmit={submit}>
+    <div className={styles["login"]}>
       <Heading>Вход</Heading>
-      <form className={styles["form"]}>
+      {error && <div className={styles["error"]}>{error}</div>}
+      <form className={styles["form"]} onSubmit={submit}>
         <div className={styles["field"]}>
           <label htmlFor="email">Ваш email</label>
-          <Input id="email" type="email" placeholder="Email" />
+          <Input id="email" type="email" name="email" placeholder="Email" />
         </div>
         <div className={styles["field"]}>
           <label htmlFor="password">Ваш пароль</label>
-          <Input id="password" type="password" placeholder="Password" />
+          <Input
+            id="password"
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
         </div>
         <Button appearence="big">Вход</Button>
       </form>
