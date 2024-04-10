85331eda3bac258e3caaadfc6832aba31d52e4ff
Yuliya <iksulia1@gmail.com>
2024-04-10 17:10:39 +0300

    create user

--- server/app.js
+++ server/app.js
@@ -3,11 +3,14 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const fs = require("fs");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -15,8 +18,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// Раздавать статические файлы из папки 'uploads'
+app.use("/uploads", express.static("uploads"));
+
+app.use("/api", indexRouter);
+
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const UserController = require("../controllers/user.controller");
+const multer = require("multer");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+const uploadDestination = "uploads";
+
+// Показываем, где хранить загружаемые файлы
+const storage = multer.diskStorage({
+  destination: uploadDestination,
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
+
+const upload = multer({ storage: storage });
+
+//User routs
+router.post("/register", UserController.register);
+router.post("/login", UserController.login);
+router.get("/current", UserController.current);
+router.get("/users/:id", UserController.getUserById);
+router.put("/users/:id", UserController.updateUser);
 
 module.exports = router;
